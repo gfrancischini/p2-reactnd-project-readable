7282d172ccef68073cf8c28fa35d55a91923b2b7
Gabriel Parelli Francischini <gpfrancischini@gmail.com>
2018-02-08 18:03:29 -0200

    trying to fix heroku build

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -88,10 +88,10 @@ module.exports = {
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
-      'components': path.resolve('src/components'),
-      'containers': path.resolve('src/containers'),
-      'services': path.resolve('src/services'),
-      'utils': path.resolve('src/utils'),
+      'components': path.resolve('./src/components'),
+      'containers': path.resolve('./src/containers'),
+      'services': path.resolve('./src/services'),
+      'utils': path.resolve('./src/utils'),
     },
     plugins: [
       // Prevents users from importing files from outside of src/ (or node_modules/).
